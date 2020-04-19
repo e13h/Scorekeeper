0ce9d6ed55bebd4a442f3e5e03f1a99f882059b8
Evan Smith <evanphilipsmith@gmail.com>
2020-04-19 17:13:34 -0600

    Initial Commit

---
+++ Scorekeeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scorekeeper
+//
+//  Created by Evan Smith on 4/19/20.
+//  Copyright © 2020 Evan Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
